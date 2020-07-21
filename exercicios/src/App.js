de7d5f3dfa876502fb1151c4a86871de87b975eb
rafaelturse <rafaelturse@hotmail.com>
2020-07-21 11:51:22 -0400

    aula 14

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,8 +1,4 @@
 import React from 'react'
 import {Text} from 'react-native'
 
-function App() {
-    return <Text>Primeiro Componente!</Text>
-}
-
-export default App
+export default () => <Text>Primeiro Componente!</Text>
