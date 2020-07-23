2bfeec8d187714c9a618800604ea0930f89b5d9a
rafaelturse <rafaelturse@hotmail.com>
2020-07-23 17:18:31 -0400

    aula 16

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react'
-import { SafeAreaView } from 'react-native'
+import { SafeAreaView, StyleSheet } from 'react-native'
 
 import Primeiro from './components/Primeiro'
 import X, { Comp1, Comp2 } from './components/Multi'
 
 export default () => (
-    <SafeAreaView>
+    <SafeAreaView style={style.App}>
         <X></X>
         <Comp1></Comp1>
         <Comp2></Comp2>
         <Primeiro />
     </SafeAreaView>
 )
+
+const style = StyleSheet.create({
+    App:  {
+        backgroundColor: '#FAFAFA',
+        flexGrow: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    }
+})
--- exercicios/src/components/Multi.js
+++ exercicios/src/components/Multi.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import { Text } from 'react-native'
 
+import Estilo from './estilo'
+
 
 export default function () {
-    return <Text>Comp #Oficial</Text>
+    return <Text style={Estilo.FontG} >Comp #Oficial</Text>
 }
 
 export function Comp1() {
-    return <Text>Comp #01</Text>
+    return <Text style={Estilo.FontG} >Comp #01</Text>
 }
 
 export function Comp2() {
-    return <Text>Comp #02</Text>
+    return <Text style={Estilo.FontG} >Comp #02</Text>
 }
 
+/*
 export { Comp1, Comp2 }
 export default { Comp }
+*/
